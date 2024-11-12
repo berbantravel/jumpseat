5ac3b34dceb99c2295b250369ba9f20aa803085f
SylvesterIII <admin@berbantravel.com>
2024-11-12 20:09:10 +0800

    fix: ipay88 signature

--- src/lib/ipay88.ts
+++ src/lib/ipay88.ts
@@ -1,4 +1,5 @@
 import crypto from 'crypto';
+
 interface SignatureParams {
   MerchantCode: string;
   RefNo: string;
@@ -8,12 +9,37 @@ interface SignatureParams {
 
 export function generateSignature(params: SignatureParams, merchantKey: string): string {
   const { MerchantCode, RefNo, Amount, Currency } = params;
-  
+
+  // Ensure Amount is formatted correctly (remove decimal and comma)
   const formattedAmount = Number(Amount).toFixed(2).replace(',', '').replace('.', '');
-  
+
+  // Concatenate the parameters with the merchant key (verify order in the iPay88 docs)
   const stringToHash = `${merchantKey}${MerchantCode}${RefNo}${formattedAmount}${Currency}`;
+
+  // Generate MD5 hash
+  const signature = crypto.createHash('md5').update(stringToHash).digest('hex').toUpperCase();
   
-  // Generate SHA256 hash
-  const signature = crypto.createHash('sha256').update(stringToHash).digest('hex');
   return signature;
 }
+
+
+
+// import crypto from 'crypto';
+// interface SignatureParams {
+//   MerchantCode: string;
+//   RefNo: string;
+//   Amount: string;
+//   Currency: string;
+// }
+
+// export function generateSignature(params: SignatureParams, merchantKey: string): string {
+//   const { MerchantCode, RefNo, Amount, Currency } = params;
+  
+//   const formattedAmount = Number(Amount).toFixed(2).replace(',', '').replace('.', '');
+  
+//   const stringToHash = `${merchantKey}${MerchantCode}${RefNo}${formattedAmount}${Currency}`;
+  
+//   // Generate SHA256 hash
+//   const signature = crypto.createHash('sha256').update(stringToHash).digest('hex');
+//   return signature;
+// }
