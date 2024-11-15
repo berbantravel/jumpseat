450aa4f77e04b73b985aa227bd111c3df63df439
SylvesterIII <admin@berbantravel.com>
2024-11-15 22:52:19 +0800

    fix: backend

--- src/lib/ipay88.ts
+++ src/lib/ipay88.ts
@@ -1,5 +1,4 @@
 import crypto from 'crypto';
-
 interface SignatureParams {
   MerchantCode: string;
   RefNo: string;
@@ -7,22 +6,17 @@ interface SignatureParams {
   Currency: string;
 }
 
-
 export function generateSignature(params: SignatureParams, merchantKey: string): string {
   const { MerchantCode, RefNo, Amount, Currency } = params;
-
-  // Ensure Amount is formatted correctly (remove decimal and comma)
-  const formattedAmount = Number(Amount).toFixed(2).replace(',', '').replace('.', '');
-
-  // Concatenate the parameters with the merchant key (verify order in the iPay88 docs)
-  const stringToHash = `${merchantKey}${MerchantCode}${RefNo}${formattedAmount}${Currency}`;
-
-  // Generate MD5 hash
-  const signature = crypto.createHash('sha256').update(stringToHash).digest('hex').toUpperCase();
   
+  const formattedAmount = Number(Amount).toFixed(2).replace(',', '').replace('.', '');
+  
+  const stringToHash = `${merchantKey}${MerchantCode}${RefNo}${formattedAmount}${Currency}`;
+  
+  // Generate SHA256 hash
+  const signature = crypto.createHash('sha256').update(stringToHash).digest('hex');
   return signature;
 }
-
 
 
 // import crypto from 'crypto';
