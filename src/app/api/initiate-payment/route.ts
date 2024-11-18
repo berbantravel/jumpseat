b8ce8d40661dffe4c3b075bdc161c05ee58b0712
SylvesterIII <admin@berbantravel.com>
2024-11-19 03:10:58 +0800

    fix: backend

--- src/app/api/initiate-payment/route.ts
+++ src/app/api/initiate-payment/route.ts
@@ -1,4 +1,5 @@
 // app/api/initiate-payment/route.ts
+
 import { NextRequest, NextResponse } from 'next/server';
 import { generateSignature } from '@/lib/ipay88';
 
@@ -16,7 +17,7 @@ export async function POST(request: NextRequest) {
     const merchantKey = process.env.NEXT_PUBLIC_IPAY88_MERCHANT_KEY as string;
     
     // console.log(merchantKey);
-    const signature = generateSignature(merchantKey,{ MerchantCode, RefNo, Amount, Currency });
+    const signature = generateSignature(merchantKey, { MerchantCode, RefNo, Amount, Currency });
     // console.log(signature);
 
     const paymentPayload = {
@@ -26,6 +27,34 @@ export async function POST(request: NextRequest) {
 
     return NextResponse.json({ success: true, payload: paymentPayload });
 }
+
+// import { NextRequest, NextResponse } from 'next/server';
+// import { generateSignature } from '@/lib/ipay88';
+
+// interface PaymentRequestBody {
+//     MerchantCode: string;
+//     RefNo: string;
+//     Amount: string;
+//     Currency: string;
+//     [key: string]: string;
+// }
+
+// export async function POST(request: NextRequest) {
+//     const body: PaymentRequestBody = await request.json();
+//     const { MerchantCode, RefNo, Amount, Currency } = body;
+//     const merchantKey = process.env.NEXT_PUBLIC_IPAY88_MERCHANT_KEY as string;
+    
+//     // console.log(merchantKey);
+//     const signature = generateSignature({ MerchantCode, RefNo, Amount, Currency }, merchantKey);
+//     // console.log(signature);
+
+//     const paymentPayload = {
+//         ...body,
+//         Signature: signature
+//     };
+
+//     return NextResponse.json({ success: true, payload: paymentPayload });
+// }
 
 
 // app/api/initiate-payment/route.ts
--- src/lib/ipay88.ts
+++ src/lib/ipay88.ts
@@ -36,3 +36,22 @@ export function generateSignature(merchantKey: string, params: SignatureParams):
 //   const signature = crypto.createHash('sha256').update(stringToHash).digest('hex');
 //   return signature;
 // }
+// import crypto from 'crypto';
+// interface SignatureParams {
+//   MerchantCode: string;
+//   RefNo: string;
+//   Amount: string;
+//   Currency: string;
+// }
+
+// export function generateSignature(params: SignatureParams, merchantKey: string): string {
+//   const { MerchantCode, RefNo, Amount, Currency } = params;
+  
+//   const formattedAmount = Number(Amount).toFixed(2).replace(',', '').replace('.', '');
+  
+//   const stringToHash = `${merchantKey}${MerchantCode}${RefNo}${formattedAmount}${Currency}`;
+  
+//   // Generate SHA256 hash
+//   const signature = crypto.createHash('sha256').update(stringToHash).digest('hex');
+//   return signature;
+// }
