7fa51bc0f9b213cd3195a9c8c765bd23fcfbebb7
sylvester-koda <sylvester.rubioIII@kodakollectiv.com>
2025-01-17 09:53:51 +0800

    feat: testing package BPI online transfer

--- src/constants/destinations.ts
+++ src/constants/destinations.ts
@@ -76,7 +76,7 @@ export const destinations = [
   {
     name: 'TEST PACKAGE',
     description: "HEART-WARMING DESTINATION THAT UNCOVERS NATURE'S GLAMOR",
-    price: '3000',
+    price: '1',
     href: '#',
     imageSrc:
       'https://images.pexels.com/photos/2407265/pexels-photo-2407265.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
